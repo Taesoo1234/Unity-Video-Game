ceeb4a96e7d0ed5649a92f8ead58c3a34660d44f
Taesoo1234 <101220248+Taesoo1234@users.noreply.github.com>
2022-03-30 11:08:02 +1300

    1
    Character Jumping

---
+++ Unity Video Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    private Animator playerAnim;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnim.SetTrigger("Jump_AR_Anim");
+        }
+    }
+}
